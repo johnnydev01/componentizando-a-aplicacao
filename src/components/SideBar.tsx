8ec2413465d2e947912ac50b4d99c9a10b814fe5
Johnny Ferreira da Silva <joferres@everis.com>
2021-07-10 15:37:17 -0300

    componentizando a aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { Button } from "./Button"
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void
+}
+
+
+export function SideBar(props: SideBarProps) {
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+  </nav>
+  )
 }
